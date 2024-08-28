8ac7f4990fd5915596f57a46f5f1d900f49f779c
MOHD YUSUF <mohdyusufr@gmail.com>
2024-08-28 13:08:15 +0530

    Upload program02.py

---
+++ Week02/program02.py
@@ -0,0 +1,12 @@
+# Write a program to count the total number of digits in a number using a while loop.
+
+num = input("Enter your number : ")
+print(len(num))
+temp = int(num)
+count = 0
+while temp > 0:
+    if temp :
+        temp //= 10
+        count+=1
+
+print(f"Total number of digits in {num} are : {count}")
