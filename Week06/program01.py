baf32569c65693f2c45763e294cf7c47d723facf
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-09-05 13:03:42 +0530

    upload program01.py

---
+++ Week06/program01.py
@@ -0,0 +1,12 @@
+'''
+Write a Python program that inputs two tuples and creates a third that contains 
+all elements of the first followed by all elements of the second. (You may use 
+other data types such as lists etc. to make your program work) 
+'''
+
+list = []
+tuple = (10,15,20,25,30,35,40)
+tuple2 = (11,16,21,26,31,36,41)
+list.append(tuple)
+list.append(tuple2)
+print(list)
